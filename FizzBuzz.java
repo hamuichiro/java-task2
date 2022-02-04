1301435c9f66dfd4fb6b49f01b6194c078709d39
hamuichiro <hamuichiro8616@icloud.com>
2022-02-04 14:52:05 +0900

    first commit

---
+++ FizzBuzz.java
@@ -0,0 +1,19 @@
+public class FizzBuzz{
+    public static void main(String[] args){
+        int number =  Integer.parseInt(args[0]);
+        for (int i = 1; i <= number; i ++){
+            if (i % 3 == 0){
+                if (i % 5 ==0){
+                    System.out.print("FizzBuzz");
+                }else {
+                    System.out.print("Fizz");
+                }
+            }else if (i % 5 == 0){
+                System.out.print("Buzz");
+            }else {
+                System.out.print(i);
+            }
+            System.out.print(" ");
+        }
+    }
+}
