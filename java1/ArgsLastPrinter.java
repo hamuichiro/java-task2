efc78a5998c2eb5fa2647f0e56cda570db266309
hamuichiro <hamuichiro8616@icloud.com>
2022-02-13 19:44:52 +0900

    #1

--- java1/ArgsLastPrinter.java
+++ java1/ArgsLastPrinter.java
@@ -1,8 +1,8 @@
-public class ArgsLastPrinter{
-    public static void main(String[] args){
-        if (args.length == 0){
+public class ArgsLastPrinter {
+    public static void main(String[] args) {
+        if (args.length == 0) {
             System.out.println("‚È‚µ");
-        } else{
+        } else {
             String str = args[args.length - 1];
             System.out.println(str);
         }
--- java1/FizzBuzz.java
+++ java1/FizzBuzz.java
@@ -1,20 +1,21 @@
-public class FizzBuzz{
-    public static void main(String[] args){
+public class FizzBuzz {
+    public static void main(String[] args) {
         int number = 0;
-        if (args.length != 0){
-            number =  Integer.parseInt(args[0]);
+        if (args.length != 0) {
+            number = Integer.parseInt(args[0]);
         }
-        for (int i = 1; i <= number; i ++){
-            if (i % 3 == 0){
-                System.out.print("Fizz");
+        for (int i = 1; i <= number; i ++) {
+            String str = "";
+            if (i % 3 == 0) {
+                str += "Fizz";
             }
-            if (i % 5 == 0){
-                System.out.print("Buzz");
+            if (i % 5 == 0) {
+                str += "Buzz";
             }
-            if (i % 3 != 0 && i % 5 != 0) {
-                System.out.print(i);
+            if (str == "") {
+                str += i;
             }
-            System.out.print(" ");
+            System.out.print(str + " ");
         }
     }
 }
